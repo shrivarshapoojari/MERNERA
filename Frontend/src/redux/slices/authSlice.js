6f1792f39af52d4f44076ef73f9a5b6f0fce4ca5
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-11-26 08:59:38 +0530

    Bug

--- Frontend/src/redux/slices/authSlice.js
+++ Frontend/src/redux/slices/authSlice.js
@@ -1,11 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import toast from "react-hot-toast";
 import axiosInstance  from "../../config/axiosInstance"
+
+
  
+
 const initialState={
     isLoggedIn:localStorage.getItem("isLoggedIn") || false,
     role:localStorage.getItem("role") || "",
-    data: JSON.parse(localStorage.getItem("data") )|| " "
+    data: JSON.parse(localStorage.getItem("data"))|| " "
     
 }
 //   data: localStorage.getItem('data') !== undefined ? JSON.parse(localStorage.getItem('data')) : {},
